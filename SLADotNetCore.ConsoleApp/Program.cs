5c29b71796ca9d3853ec980ae09054a1da92fb4d
Y2023010 <Y2023010@TMPC0045>
2025-06-27 16:31:42 +0630

    db connect, data read test

--- SLADotNetCore.ConsoleApp/Program.cs
+++ SLADotNetCore.ConsoleApp/Program.cs
@@ -17,13 +17,41 @@ Console.WriteLine("Hello, World!");
 
 // Ctrl + .
 
-string connectionString = "Data Source=TMPC0045\\SQLEXPRESS02;Initial Catalog=SLADotNetCore;User ID=sa;Password=root;Encrypt=True;TrustServerCertificate=True;";
-Console.WriteLine("Connection String " + connectionString);
-SqlConnection connection = new SqlConnection(connectionString);
+//string connectionString = "Data Source=TMPC0045\\SQLEXPRESS02;Initial Catalog=SLADotNetCore;User ID=sa;Password=root;Encrypt=True;TrustServerCertificate=True;";
+//Console.WriteLine("Connection String " + connectionString);
+//SqlConnection connection = new SqlConnection(connectionString);
 
-Console.WriteLine("Connection opening...");
+//Console.WriteLine("Connection opening...");
+//connection.Open();
+//Console.WriteLine("Connection opened !");
+
+//string query = @"SELECT [BlogID]
+//      ,[BlogTitle]
+//      ,[BlogAuthor]
+//      ,[BlogContent]
+//      ,[Delete_Flag]
+//  FROM [dbo].[Tbl_Blog] WHERE Delete_Flag = 0";
+//SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+//DataTable dataTable = new DataTable();
+//adapter.Fill(dataTable);
+
+//foreach (DataRow row in dataTable.Rows)
+//{
+//    Console.WriteLine(row["BlogId"]);
+//    Console.WriteLine(row["BlogAuthor"]);
+//    Console.WriteLine(row["BlogContent"]);
+//    Console.WriteLine(row["Delete_Flag"]);
+//}
+
+//Console.WriteLine("Connection closing...");
+//connection.Close();
+//Console.WriteLine("Connectiong closed !");
+
+string connectionString = "Data Source=TMPC0045\\SQLEXPRESS02;Initial Catalog=SLADotNetCore;User ID=sa;Password=root;Encrypt=True;TrustServerCertificate=True;";
+SqlConnection connection = new SqlConnection(connectionString);
 connection.Open();
-Console.WriteLine("Connection opened !");
+Console.WriteLine("connection opened !");
 
 string query = @"SELECT [BlogID]
       ,[BlogTitle]
@@ -31,22 +59,23 @@ string query = @"SELECT [BlogID]
       ,[BlogContent]
       ,[Delete_Flag]
   FROM [dbo].[Tbl_Blog] WHERE Delete_Flag = 0";
+
 SqlCommand cmd = new SqlCommand(query, connection);
 SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-DataTable dataTable = new DataTable();
-adapter.Fill(dataTable);
+DataTable dataTb = new DataTable();
+adapter.Fill(dataTb);
 
-foreach (DataRow row in dataTable.Rows)
+foreach (DataRow row in dataTb.Rows)
 {
     Console.WriteLine(row["BlogId"]);
+    Console.WriteLine(row["BlogTitle"]);
     Console.WriteLine(row["BlogAuthor"]);
     Console.WriteLine(row["BlogContent"]);
     Console.WriteLine(row["Delete_Flag"]);
 }
 
-Console.WriteLine("Connection closing...");
 connection.Close();
-Console.WriteLine("Connectiong closed !");
+Console.WriteLine("Connectiong Closed !");
 
 Console.ReadKey();
 

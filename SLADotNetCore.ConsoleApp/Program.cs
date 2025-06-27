27e1a11a9d6e175d8c563d5bd29e5d8cd4ebf8a4
Y2023010 <Y2023010@TMPC0045>
2025-06-27 16:17:44 +0630

    db connect, data read

--- SLADotNetCore.ConsoleApp/Program.cs
+++ SLADotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+using System.Data;
+
 Console.WriteLine("Hello, World!");
+//Console.ReadLine();
+//Console.ReadKey();
+
+// mk => markdown
+
+// C# <=> Database
+// ADO.NET
+// Dapper (ORM)
+// EFCore / Entity Framework (ORM)
+
+// nuget
+
+// Ctrl + .
+
+string connectionString = "Data Source=TMPC0045\\SQLEXPRESS02;Initial Catalog=SLADotNetCore;User ID=sa;Password=root;Encrypt=True;TrustServerCertificate=True;";
+Console.WriteLine("Connection String " + connectionString);
+SqlConnection connection = new SqlConnection(connectionString);
+
+Console.WriteLine("Connection opening...");
+connection.Open();
+Console.WriteLine("Connection opened !");
+
+string query = @"SELECT [BlogID]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[Delete_Flag]
+  FROM [dbo].[Tbl_Blog] WHERE Delete_Flag = 0";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dataTable = new DataTable();
+adapter.Fill(dataTable);
+
+foreach (DataRow row in dataTable.Rows)
+{
+    Console.WriteLine(row["BlogId"]);
+    Console.WriteLine(row["BlogAuthor"]);
+    Console.WriteLine(row["BlogContent"]);
+    Console.WriteLine(row["Delete_Flag"]);
+}
+
+Console.WriteLine("Connection closing...");
+connection.Close();
+Console.WriteLine("Connectiong closed !");
+
+Console.ReadKey();
+
